631a54b9cd6f73a9c63edaa2dd433ffc33060521
LilAliTech <lilalix20@gmail.com>
2023-10-29 18:24:07 +0000

    create/edit billboard page

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,7 +1,23 @@
-const BillboardPage = () => {
+import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
+
+const BillboardPage = async ({
+    params
+}: {
+    params: {billboardId: string}
+}) => {
+    const billboard = await prismadb.billboard.findUnique({
+        where: {
+            id: params.billboardId
+        }
+    });
+
+
     return ( 
-        <div>
-            Billboard page
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardForm initialData={billboard} /> 
+            </div>
         </div>
      );
 }
