d6f43bb7e042dfba823482f8ceba61390dadd082
LilAliTech <lilalix20@gmail.com>
2023-11-23 10:45:06 +0000

    Dashboard Overview

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ModalProvider } from '@/providers/modal-provider'
 import { ToastProvider } from '@/providers/toast-provider'
 
 import './globals.css'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,9 +23,11 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
         <ToastProvider />
         <ModalProvider />
           {children}
+        </ThemeProvider>
       </body>
     </html>
     </ClerkProvider>
