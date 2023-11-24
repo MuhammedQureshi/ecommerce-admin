3183f435b417dca8101d5541fc771bd6760ef608
LilAliTech <lilalix20@gmail.com>
2023-11-24 13:59:50 +0000

    Image upload preset upd

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="ak4gvhd8">
         {({ open }) => {
           const onClick = () => {
             open();
